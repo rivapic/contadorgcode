04073e3d54111027bd64a3e8e61d7547b05c75fd
Javier Gonzalez riva <rivapic@gmail.com>
2021-07-15 10:57:44 +0200

    Add files via upload

---
+++ contadorgcode.ps1
@@ -0,0 +1,7 @@
+﻿## Contador de filamento por Javier Riva rivapic@gmail.com
+$gcodefiles = (Get-ChildItem . | Where-Object {$_.Extension -eq '.gcode'}).FullName
+[int]$suma =0
+ForEach ($gcodefiles in $gcodefiles) {
+    $suma= ($suma + [int](((Get-content $gcodefiles | Select -Index 2).TrimStart(";Filament used: ").TrimEnd("m")) -replace '\D', ''))
+}
+Write-Output ("metros de filamento gastados = "+ ($suma / 100000)+"m ")
